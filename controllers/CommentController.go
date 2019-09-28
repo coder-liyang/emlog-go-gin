9ae21e68b0cd67c15d07488d3a4ef78dcc9f53e4
liyang <liyangcoder@163.com>
2019-09-28 22:41:39 +0800

    评论支持分页

--- controllers/CommentController.go
+++ controllers/CommentController.go
@@ -9,7 +9,7 @@ import (
 
 func CommentRows(c *gin.Context) {
 	gid, _ := strconv.ParseInt(c.Param("gid"), 10, 64)
-	page, _ := strconv.ParseInt(c.Param("page"), 10, 64)
+	page, _ := strconv.ParseInt(c.Query("page"), 10, 64)
 	comments := models.GetCommentList(gid, 0, page)
 	c.JSON(http.StatusOK, comments)
 }
--- models/comment.go
+++ models/comment.go
@@ -1,26 +1,26 @@
 package models
 
 type Comment struct {
-	Cid     int64
-	Gid     int64
-	Pid     int64
-	Date    int64
-	Poster  string
-	Comment string
-	Mail    string
-	Url     string
-	Ip      string
-	Hide    string
-	ChildComments []Comment
+	Cid           int64     `json:"cid"`
+	Gid           int64     `json:"gid"`
+	Pid           int64     `json:"pid"`
+	Date          int64     `json:"date"`
+	Poster        string    `json:"poster"`
+	Comment       string    `json:"comment"`
+	Mail          string    `json:"mail"`
+	Url           string    `json:"url"`
+	Ip            string    `json:"ip"`
+	Hide          string    `json:"hide"`
+	ChildComments []Comment `json:"child_comments"`
 }
 
 func GetCommentList(gid int64, pid int64, page int64) []Comment {
 	var limit int64 = 10
-	var commentList  []Comment
+	var commentList []Comment
 	Orm.
 		Where("gid = ? and pid = ? and hide = ?", gid, pid, "n").
 		Limit(limit).
-		Offset((page-1) * limit).
+		Offset((page - 1) * limit).
 		Find(&commentList)
 
 	return commentList
