d31e9e9190b9a0379a4a7cfccf12f9de3bc775b6
liyang <liyangcoder@163.com>
2019-09-29 09:38:23 +0800

    修改了注释

--- routes/api.go
+++ routes/api.go
@@ -7,9 +7,9 @@ import (
 
 func RegisterApi(r *gin.Engine) {
 	api := r.Group("api/")
-	//文章列表
-	api.GET("blog/row/:gid", controllers.BlogRow)
 	//一篇文章
+	api.GET("blog/row/:gid", controllers.BlogRow)
+	//文章列表
 	api.GET("blog/rows", controllers.BlogRows)
 	//文章评论
 	api.GET("comment/rows/:gid", controllers.CommentRows)
