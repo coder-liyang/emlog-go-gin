5326cc62700f95fdd8dc809b1d5f715c5884cc78
liyang <liyangcoder@163.com>
2019-10-04 11:12:47 +0800

    导航支持查询子查询

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module emlog-go-gin
 go 1.13
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/gin-gonic/gin v1.4.0
 	github.com/jinzhu/gorm v1.9.10
 )
--- models/navi.go
+++ models/navi.go
@@ -1,19 +1,18 @@
 package models
 
 type Navi struct {
-	Id int64
-	Naviname string
-	Url string
-	Newtab string
-	Hide string
-	Taxis int64
-	Pid int64
-	Isdefault string
-	Type int64 //1,2,3:系统;4:分类;5:页面;0:自定义
-	Type_id int64
-	ChildNavi []Navi
+	Id        int64  `json:"id"`
+	Naviname  string `json:"naviname"`
+	Url       string `json:"url"`
+	Newtab    string `json:"newtab"`
+	Hide      string `json:"hide"`
+	Taxis     int64  `json:"taxis"`
+	Pid       int64  `json:"pid"`
+	Isdefault string `json:"isdefault"`
+	Type      int64  `json:"type"` //1,2,3:系统;4:分类;5:页面;0:自定义
+	Type_id   int64  `json:"type_id"`
+	ChildNavi []Navi `json:"child_navi"`
 }
-
 
 //导航列表
 func GetNaviList(pid int64) []Navi {
@@ -22,10 +21,27 @@ func GetNaviList(pid int64) []Navi {
 	qs.Find(&naviList)
 	//查询有子导航
 	for index, item := range naviList {
-		//TODO 如果item.type=4,则此导航下是分类,需要查询分类信息
 		childNaviList := GetNaviList(item.Id)
 		if len(childNaviList) > 0 {
 			naviList[index].ChildNavi = childNaviList
+		} else if item.Type == 4 {
+			//如果item.type=4,则此导航下是分类,需要查询分类信息
+			sort := GetSortList(item.Type_id)
+			for _, sortItem := range sort {
+				naviList[index].ChildNavi = append(naviList[index].ChildNavi, Navi{
+					Id:        0,
+					Naviname:  sortItem.Sortname,
+					Url:       "",
+					Newtab:    "n",
+					Hide:      "n",
+					Taxis:     0,
+					Pid:       item.Id,
+					Isdefault: "",
+					Type:      4,
+					Type_id:   0,
+					ChildNavi: nil,
+				})
+			}
 		}
 	}
 	return naviList
