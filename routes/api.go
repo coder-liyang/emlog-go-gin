d4612f19db6a37c5dc647e0e20ab4bc2a4f75a68
liyang <liyangcoder@163.com>
2019-10-17 21:44:36 +0800

    接口支持跨域

--- routes/api.go
+++ routes/api.go
@@ -3,9 +3,11 @@ package routes
 import (
 	"emlog-go-gin/controllers"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func RegisterApi(r *gin.Engine) {
+	r.Use(Cors())
 	api := r.Group("api")
 	//一篇文章
 	api.GET("blog/row/:gid", controllers.BlogRow)
@@ -19,4 +21,19 @@ func RegisterApi(r *gin.Engine) {
 	api.GET("navi/rows", controllers.NaviRows)
 	//友情链接列表
 	api.GET("link/rows", controllers.LinkRows)
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token,Authorization,Token")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Header("Access-Control-Allow-Expose-Headers", "Content-Length,Access-Control-Allow-Origin,Access-Control-Allow-Headers,Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+	}
 }
